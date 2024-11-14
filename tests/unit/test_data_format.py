46e3671d3367b1e4589ea35002e6aeee9c8ad85c
zero88 <10863525+zero88@users.noreply.github.com>
2024-11-14 19:51:36 +0700

    chore(test): migrate to pytest

--- tests/unit/test_data_format.py
+++ tests/unit/test_data_format.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
+import pytest
+
 from ExcelRobot.utils import DateFormat
-from nose.tools import eq_
-from parameterized import parameterized
 
 
-@parameterized([
-    ('yyyy-mm-dd', '%Y-%m-%d'),
-    ('yyyymmdd', '%Y%m%d'),
-    ('dd/mm/yyyy', '%d/%m/%Y'),
-    ('mmm, dd yyyy', '%b, %d %Y'),
-    ('m/d/yy', '%-m/%-d/%y'),
-    ('HH:MM:SS', '%H:%M:%S'),
-    ('HH.MM.SS', '%H.%M.%S'),
-    ('HH:MM:SS AM/PM', '%I:%M:%S %p'),
-    ('HHMMSS-A/P', '%I%M%S-%p'),
-    ('yyyy mm dd HH:MM:SS', '%Y %m %d %H:%M:%S'),
-])
+@pytest.mark.parametrize(
+    'pattern,expected',
+    [
+        ('yyyy-mm-dd', '%Y-%m-%d'),
+        ('yyyymmdd', '%Y%m%d'),
+        ('dd/mm/yyyy', '%d/%m/%Y'),
+        ('mmm, dd yyyy', '%b, %d %Y'),
+        ('m/d/yy', '%-m/%-d/%y'),
+        ('HH:MM:SS', '%H:%M:%S'),
+        ('HH.MM.SS', '%H.%M.%S'),
+        ('HH:MM:SS AM/PM', '%I:%M:%S %p'),
+        ('HHMMSS-A/P', '%I%M%S-%p'),
+        ('yyyy mm dd HH:MM:SS', '%Y %m %d %H:%M:%S'),
+    ],
+)
 def test_date_format(pattern, expected):
-    eq_(expected, DateFormat.excel2python_format(pattern))
+    assert DateFormat.excel2python_format(pattern) == expected
--- tests/unit/test_reader.py
+++ tests/unit/test_reader.py
@@ -2,144 +2,161 @@
 import os.path as path
 from datetime import datetime
 
+import pytest
 from six import PY2
 
 from ExcelRobot.reader import ExcelReader
 from ExcelRobot.utils import DataType
-from nose.tools import assert_in, eq_, raises
-from parameterized import parameterized
 
 CURRENT_DIR = path.dirname(path.abspath(__file__))
 DATA_DIR = path.join(CURRENT_DIR, '../data')
 
 
-@raises(ValueError)
 def test_open_not_valid():
-    ExcelReader(path.join(DATA_DIR, 'a.txt'))
+    with pytest.raises(ValueError):
+        ExcelReader(path.join(DATA_DIR, 'a.txt'))
 
 
-@raises(IOError if PY2 else FileNotFoundError)
 def test_open_not_found():
-    ExcelReader(path.join(DATA_DIR, 'a.xls'))
+    with pytest.raises(IOError if PY2 else FileNotFoundError):
+        ExcelReader(path.join(DATA_DIR, 'a.xls'))
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 5),
-    ('ExcelRobotTest.xlsx', 5)
-])
+@pytest.mark.parametrize('input_file,expected', [('ExcelRobotTest.xls', 5), ('ExcelRobotTest.xlsx', 5)])
 def test_open_success(input_file, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.get_number_of_sheets())
+    assert reader.get_number_of_sheets() == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1')
-])
+@pytest.mark.parametrize(
+    'input_file,expected',
+    [('ExcelRobotTest.xls', 'TestSheet1'), ('ExcelRobotTest.xlsx', 'TestSheet1')],
+)
 def test_sheet_name(input_file, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    assert_in(expected, reader.get_sheet_names())
+    assert expected in reader.get_sheet_names()
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1', 2, 3),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 2, 3)
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, col_count, row_count',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet1', 2, 3),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 2, 3),
+    ],
+)
 def test_sheet_size(input_file, sheet_name, col_count, row_count):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(col_count, reader.get_column_count(sheet_name))
-    eq_(row_count, reader.get_row_count(sheet_name))
+    assert reader.get_column_count(sheet_name) == col_count
+    assert reader.get_row_count(sheet_name) == row_count
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1', 0, [('A1', 'This is a test sheet'), ('A2', 'User1'), ('A3', 'User2')]),
-    ('ExcelRobotTest.xls', 'TestSheet1', 1, [('B1', 'Points'), ('B2', 57), ('B3', 5178)]),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 0, [('A1', 'This is a test sheet'), ('A2', 'User1'), ('A3', 'User2')]),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 1, [('B1', 'Points'), ('B2', 57), ('B3', 5178)]),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, column, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet1', 0, [('A1', 'This is a test sheet'), ('A2', 'User1'), ('A3', 'User2')]),
+        ('ExcelRobotTest.xls', 'TestSheet1', 1, [('B1', 'Points'), ('B2', 57), ('B3', 5178)]),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 0, [('A1', 'This is a test sheet'), ('A2', 'User1'), ('A3', 'User2')]),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 1, [('B1', 'Points'), ('B2', 57), ('B3', 5178)]),
+    ],
+)
 def test_get_col_values(input_file, sheet_name, column, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.get_column_values(sheet_name, column))
+    assert reader.get_column_values(sheet_name, column) == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet2', 0, [('A1', 'This is a test sheet'), ('B1', 'Date of Birth')]),
-    ('ExcelRobotTest.xls', 'TestSheet2', 1, [('A2', 'User3'), ('B2', '23.8.1982')]),
-    ('ExcelRobotTest.xlsx', 'TestSheet2', 0, [('A1', 'This is a test sheet'), ('B1', 'Date of Birth')]),
-    ('ExcelRobotTest.xlsx', 'TestSheet2', 1, [('A2', 'User3'), ('B2', '23.8.1982')]),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, column, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet2', 0, [('A1', 'This is a test sheet'), ('B1', 'Date of Birth')]),
+        ('ExcelRobotTest.xls', 'TestSheet2', 1, [('A2', 'User3'), ('B2', '23.8.1982')]),
+        ('ExcelRobotTest.xlsx', 'TestSheet2', 0, [('A1', 'This is a test sheet'), ('B1', 'Date of Birth')]),
+        ('ExcelRobotTest.xlsx', 'TestSheet2', 1, [('A2', 'User3'), ('B2', '23.8.1982')]),
+    ],
+)
 def test_get_row_values(input_file, sheet_name, column, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.get_row_values(sheet_name, column))
+    assert reader.get_row_values(sheet_name, column) == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1', 'a2', 'User1'),
-    ('ExcelRobotTest.xls', 'TestSheet1', 'B2', '57.00'),
-    ('ExcelRobotTest.xls', 'TestSheet2', 'B2', '23.8.1982'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 'A2', 'User1'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 'B2', '57.00'),
-    ('ExcelRobotTest.xlsx', 'TestSheet2', 'B2', '23.8.1982'),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, cell_name, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet1', 'a2', 'User1'),
+        ('ExcelRobotTest.xls', 'TestSheet1', 'B2', '57.00'),
+        ('ExcelRobotTest.xls', 'TestSheet2', 'B2', '23.8.1982'),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 'A2', 'User1'),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 'B2', '57.00'),
+        ('ExcelRobotTest.xlsx', 'TestSheet2', 'B2', '23.8.1982'),
+    ],
+)
 def test_get_cell_value_by_name(input_file, sheet_name, cell_name, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.read_cell_data_by_name(sheet_name, cell_name))
+    assert reader.read_cell_data_by_name(sheet_name, cell_name) == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1', 0, 1, None, 'User1'),
-    ('ExcelRobotTest.xls', 'TestSheet1', 1, 1, DataType.NUMBER.name, '57.00'),
-    ('ExcelRobotTest.xls', 'TestSheet2', 1, 1, DataType.TEXT.name, '23.8.1982'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, DataType.DATE.name, '1982-05-14'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, DataType.BOOL.name, 'Yes'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 6, 1, DataType.NUMBER.name, '7.50'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 7, 1, DataType.TIME.name, '08:00:00 AM'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 8, 1, DataType.DATE_TIME.name, '2018-01-02 22:00'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 0, 1, None, 'User1'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 1, 1, DataType.NUMBER.name, '57.00'),
-    ('ExcelRobotTest.xlsx', 'TestSheet2', 1, 1, DataType.TEXT.name, '23.8.1982'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, DataType.DATE.name, '1982-05-14'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, DataType.BOOL.name, 'Yes'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 6, 1, DataType.NUMBER.name, '7.50'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 7, 1, DataType.TIME.name, '08:00:00 AM'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 8, 1, DataType.DATE_TIME.name, '2018-01-02 22:00'),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, col, row, data_type, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet1', 0, 1, None, 'User1'),
+        ('ExcelRobotTest.xls', 'TestSheet1', 1, 1, DataType.NUMBER.name, '57.00'),
+        ('ExcelRobotTest.xls', 'TestSheet2', 1, 1, DataType.TEXT.name, '23.8.1982'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, DataType.DATE.name, '1982-05-14'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, DataType.BOOL.name, 'Yes'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 6, 1, DataType.NUMBER.name, '7.50'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 7, 1, DataType.TIME.name, '08:00:00 AM'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 8, 1, DataType.DATE_TIME.name, '2018-01-02 22:00'),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 0, 1, None, 'User1'),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 1, 1, DataType.NUMBER.name, '57.00'),
+        ('ExcelRobotTest.xlsx', 'TestSheet2', 1, 1, DataType.TEXT.name, '23.8.1982'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, DataType.DATE.name, '1982-05-14'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, DataType.BOOL.name, 'Yes'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 6, 1, DataType.NUMBER.name, '7.50'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 7, 1, DataType.TIME.name, '08:00:00 AM'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 8, 1, DataType.DATE_TIME.name, '2018-01-02 22:00'),
+    ],
+)
 def test_get_cell_value_by_coord(input_file, sheet_name, col, row, data_type, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.read_cell_data(sheet_name, col, row, data_type=data_type))
+    assert reader.read_cell_data(sheet_name, col, row, data_type=data_type) == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet1', 0, 1, 'User1'),
-    ('ExcelRobotTest.xls', 'TestSheet1', 1, 1, 57),
-    ('ExcelRobotTest.xls', 'TestSheet2', 1, 1, '23.8.1982'),
-    ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, datetime(1982, 5, 14)),
-    ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, True),
-    ('ExcelRobotTest.xls', 'TestSheet3', 6, 1, 7.5),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 0, 1, 'User1'),
-    ('ExcelRobotTest.xlsx', 'TestSheet1', 1, 1, 57),
-    ('ExcelRobotTest.xlsx', 'TestSheet2', 1, 1, '23.8.1982'),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, datetime(1982, 5, 14)),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 6, 1, 7.5),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, col, row, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet1', 0, 1, 'User1'),
+        ('ExcelRobotTest.xls', 'TestSheet1', 1, 1, 57),
+        ('ExcelRobotTest.xls', 'TestSheet2', 1, 1, '23.8.1982'),
+        ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, datetime(1982, 5, 14)),
+        ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, True),
+        ('ExcelRobotTest.xls', 'TestSheet3', 6, 1, 7.5),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 0, 1, 'User1'),
+        ('ExcelRobotTest.xlsx', 'TestSheet1', 1, 1, 57),
+        ('ExcelRobotTest.xlsx', 'TestSheet2', 1, 1, '23.8.1982'),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, datetime(1982, 5, 14)),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 6, 1, 7.5),
+    ],
+)
 def test_get_cell_raw_value_by_coord(input_file, sheet_name, col, row, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.read_cell_data(sheet_name, col, row, use_format=False))
+    assert reader.read_cell_data(sheet_name, col, row, use_format=False) == expected
 
 
-@parameterized([
-    ('ExcelRobotTest.xls', 'TestSheet3', 0, 1, DataType.NUMBER.name, True),
-    ('ExcelRobotTest.xls', 'TestSheet3', 1, 1, DataType.TEXT.name, True),
-    ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, DataType.DATE.name, True),
-    ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, DataType.BOOL.name, True),
-    ('ExcelRobotTest.xls', 'TestSheet3', 5, 1, DataType.EMPTY.name, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 0, 1, DataType.NUMBER.name, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 1, 1, DataType.TEXT.name, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, DataType.DATE.name, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, DataType.BOOL.name, True),
-    ('ExcelRobotTest.xlsx', 'TestSheet3', 5, 1, DataType.EMPTY.name, True),
-])
+@pytest.mark.parametrize(
+    'input_file, sheet_name, col, row, data_type, expected',
+    [
+        ('ExcelRobotTest.xls', 'TestSheet3', 0, 1, DataType.NUMBER.name, True),
+        ('ExcelRobotTest.xls', 'TestSheet3', 1, 1, DataType.TEXT.name, True),
+        ('ExcelRobotTest.xls', 'TestSheet3', 2, 1, DataType.DATE.name, True),
+        ('ExcelRobotTest.xls', 'TestSheet3', 3, 1, DataType.BOOL.name, True),
+        ('ExcelRobotTest.xls', 'TestSheet3', 5, 1, DataType.EMPTY.name, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 0, 1, DataType.NUMBER.name, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 1, 1, DataType.TEXT.name, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 2, 1, DataType.DATE.name, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 3, 1, DataType.BOOL.name, True),
+        ('ExcelRobotTest.xlsx', 'TestSheet3', 5, 1, DataType.EMPTY.name, True),
+    ],
+)
 def test_check_cell_type(input_file, sheet_name, col, row, data_type, expected):
     reader = ExcelReader(path.join(DATA_DIR, input_file))
-    eq_(expected, reader.check_cell_type(sheet_name, col, row, data_type))
+    assert reader.check_cell_type(sheet_name, col, row, data_type) == expected
